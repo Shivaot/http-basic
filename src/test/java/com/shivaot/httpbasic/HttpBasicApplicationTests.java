b5fa3a7890e1f784077165efde30fd5b135ae908
Shivaot <shivatiwari757@gmail.com>
2019-10-28 18:57:16 +0530

    Initial commit

---
+++ src/test/java/com/shivaot/httpbasic/HttpBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivaot.httpbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
